777258826b9f43e0dad80deceab0b3c20c1ff73f
gaoyehua <1776654200@qq.com>
2016-07-16 01:35:28 +0800

    更新开机界面

--- app/src/main/java/com/gaoyehau/watchdog/SpalshActivity.java
+++ app/src/main/java/com/gaoyehau/watchdog/SpalshActivity.java
@@ -1,13 +1,17 @@
 package com.gaoyehau.watchdog;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class SpalshActivity extends AppCompatActivity {
+public class SpalshActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_spalsh);
+
     }
 }
